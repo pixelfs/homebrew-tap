b9cd26dca29083a7a2b4e36e83ce0c6548e00ba1
ZiHang Gao <hello@gaozihang.com>
2025-02-03 20:35:35 +0800

    Brew formula update for pixelfs version v1.0.0

--- pixelfs.rb
+++ pixelfs.rb
@@ -11,7 +11,7 @@ class Pixelfs < Formula
   on_macos do
     on_intel do
       url "https://github.com/pixelfs/pixelfs/releases/download/v1.0.0/pixelfs_darwin_amd64_1.0.0.tar.gz"
-      sha256 "4c3e32a5f11257aab0018bba977a6de562d4f8cc70debc0aa610d9da83659d52"
+      sha256 "458e73ce0202db2a46bb0c4e0ae70d9f12e4d4ce370ea2d373adafb423257bc4"
 
       def install
         bin.install "pixelfs"
@@ -19,7 +19,7 @@ class Pixelfs < Formula
     end
     on_arm do
       url "https://github.com/pixelfs/pixelfs/releases/download/v1.0.0/pixelfs_darwin_arm64_1.0.0.tar.gz"
-      sha256 "81b40d08b5df85b3d4faa0f837c1e5e66fbd51603060a3247a59758631fb306e"
+      sha256 "1450f8a798c3cf9df331ec7b8ad1047cf277f4e8628222e60b556a093de80e17"
 
       def install
         bin.install "pixelfs"
@@ -31,7 +31,7 @@ class Pixelfs < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/pixelfs/pixelfs/releases/download/v1.0.0/pixelfs_linux_amd64_1.0.0.tar.gz"
-        sha256 "2c6fba6d98b8a06bf8618356fee85c405bb16eef86569f4c1391ad1ed9ef4928"
+        sha256 "bbacc8f1303fa0a532accd017ca33ad37251568f9508479cb179228429cc40e1"
 
         def install
           bin.install "pixelfs"
@@ -41,7 +41,7 @@ class Pixelfs < Formula
     on_arm do
       if !Hardware::CPU.is_64_bit?
         url "https://github.com/pixelfs/pixelfs/releases/download/v1.0.0/pixelfs_linux_armv7_1.0.0.tar.gz"
-        sha256 "9fa743cc31424719c264f81857c538222b1f933d065d6222948073bf3f804290"
+        sha256 "26b72a483c6c69c59a98043a12caf1fdba9efda37cae4d26e30026967ab6c471"
 
         def install
           bin.install "pixelfs"
@@ -51,7 +51,7 @@ class Pixelfs < Formula
     on_arm do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/pixelfs/pixelfs/releases/download/v1.0.0/pixelfs_linux_arm64_1.0.0.tar.gz"
-        sha256 "e48908c90b2ef9e891f6447c4fb72a3d3bbb3b37b7ac58bfc3bdf46c8674bba8"
+        sha256 "9d4201888efc3b64117d63c996f6d1a2ce39a6cbd6e67b57f9fb7a8b7d4325a8"
 
         def install
           bin.install "pixelfs"
